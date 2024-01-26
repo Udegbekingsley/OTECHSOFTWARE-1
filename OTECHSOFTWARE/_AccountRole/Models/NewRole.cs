22a871ee6899e1fb89f27422f1077ae2ee902b43
otechteam <140799361+otechit@users.noreply.github.com>
2024-01-26 09:05:06 +0100

    Work Through

---
+++ OTECHSOFTWARE/_AccountRole/Models/NewRole.cs
@@ -0,0 +1,16 @@
+﻿namespace OTECHSOFTWARE._AccountRole.Models
+{
+    public class NewRoleRequest
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+    }
+    public class NewRoleResponse
+    {
+        public bool succeeded { get; set; }
+        public string message { get; set; }
+        public object errors { get; set; }
+        public object data { get; set; }
+    }
+
+}
